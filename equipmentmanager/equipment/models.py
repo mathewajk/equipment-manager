39e870bc8e787e73023f5469080a8c89a7e6a876
Mathew Jacqmin-Kramer <arkram@umich.edu>
2023-07-14 12:27:26 -0400

    Init equipment app

---
+++ equipmentmanager/equipment/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+from django.utils.timezone import timezone
+
+from autoslug import AutoSlugField
+
+
---
+++ equipmentmanager/equipment/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# class AddGroupView():
+#     ...
+
+# class EditGroupView():
+#     ...
+
+# class AddMemberView():
+#     ...
+
+# class EditMemberView():
+#     ...
+
+# class AddEquipmentView():
+#     ...
+
+# class EditEquipmentView():
+#     ...
+
+# class AddPerformanceView():
+#     ...
+
+# class EditPerformanceView():
+#     ...
+
+
